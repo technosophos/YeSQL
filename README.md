d23f860ff3392121c0dcce456eed6c764070216e
Matt Butcher <technosophos@gmail.com>
2010-11-04 14:50:27 -0500

    Initial add.

---
+++ README.md
@@ -0,0 +1,18 @@
+# YeSQL
+
+YeSQL is a NoSQL-like access layer that runs atop a PDO-encapsulated SQL database.
+
+## Installation
+
+Instructions on how to install
+
+## Usage
+
+Instructions on how to use
+
+## More information
+
+Where to learn more
+
+----
+YeSQL by mbutcher (2010)
---
+++ doc/documentation.php
@@ -0,0 +1,15 @@
+<?php
+/** @mainpage YeSQL
+ * 
+ * This is the main page for documentation.
+ *
+ * @section about_package About This Software
+ *
+ * This is the programming reference for YeSQL. You will find complete documentation for 
+ * the source of this package.
+ *
+ * @section learn_more Learn More
+ *
+ * You can learn more about this package at our website.
+ */
+?>
