5c8f39d442b22204fef9b52700dc28d57f427b4f
Matt Butcher <technosophos@gmail.com>
2010-11-04 16:34:26 -0500

    Fixed broken XML.

--- test/Tests/YeSQLTest.php
+++ test/Tests/YeSQLTest.php
@@ -7,6 +7,8 @@ class YeSQLTest extends PHPUnit_Framework_TestCase {
   public function setUp() {
     $dsn = 'sqlite:./test.sq3';
     $this->pdo = new PDO($dsn);
+    
+    $this->pdo->execute(YeSQL::schema());
   }
   
   public function tearDown() {
